457ea2a116339ac3ee13ce91cd15972d93eead74
qianyri <34699877+Nrtostp@users.noreply.github.com>
2018-10-29 13:29:22 +0800

    Update Z-BOX.cpp

--- 02_String Processing/Z-BOX.cpp
+++ 02_String Processing/Z-BOX.cpp
@@ -2,6 +2,8 @@
 z[i]:string'sa[i] compare with string the length-LCP
 S=P+$+T
 z[strlen(P)+1~~strlen(P)+strlen(T)]==length(T)--'s idex -strlen(P)-1:the place P appear in T
+S=T+$+P
+the number of z[strlen(T)+1~~strlen(T)+strlen(P)]!=0:the number of T'sa[i] is P'prefix 
 */
 void z-box()
 {
