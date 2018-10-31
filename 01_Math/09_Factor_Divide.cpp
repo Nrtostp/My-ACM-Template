d9ee14a41c01d48704ae097dfc5b78787923040e
qianyri <34699877+Nrtostp@users.noreply.github.com>
2018-10-31 20:53:21 +0800

    Update 09_Factor_Divide.cpp

--- 01_Math/09_Factor_Divide.cpp
+++ 01_Math/09_Factor_Divide.cpp
@@ -6,36 +6,23 @@ void factor(int a,int b)
     for(i=1;i<=prim[0]&&prim[i]<=a;i++)
     {
         int tmp=a;
-        while(tmp)
-        {
-            fac[i]+=tmp/prim[i];
-            tmp/=prim[i];
-        }
+        while(tmp){fac[i]+=tmp/prim[i];tmp/=prim[i];}
     }
     fac[0]=i;
     for(i=1;i<=prim[0]&&prim[i]<=b;i++)
     {
         int tmp=b;
-        while(tmp)
-        {
-            fac[i]-=tmp/prim[i];
-            tmp/=prim[i];
-        }
+        while(tmp){fac[i]-=tmp/prim[i];tmp/=prim[i];}
     }
     for(i=1;i<=prim[0]&&prim[i]<=a-b;i++)
     {
         int tmp=a-b;
-        while(tmp)
-        {
-            fac[i]-=tmp/prim[i];
-            tmp/=prim[i];
-        }
+        while(tmp){fac[i]-=tmp/prim[i];tmp/=prim[i];}
     }
 }
 long long C(int a,int b)
 {
-    factor(a,b);
-    long long c=1;
+    factor(a,b);long long c=1;
     for(int i=1;i<fac[0];i++) if(fac[i]) c=c*pow(prim[i],fac[i],MOD)%MOD;
     return c;
 }
@@ -51,18 +38,10 @@ int factor(long long x)
         if(tmp%prim[i]==0)
         {
             fac[facnt][0]=prim[i];
-            while(tmp%prim[i]==0)
-            {
-                fac[facnt][1]++;
-                tmp/=prim[i];
-            }
+            while(tmp%prim[i]==0){fac[facnt][1]++;tmp/=prim[i];}
             facnt++;
         }
     }
-    if(tmp!=1)
-    {
-        fac[facnt][0]=tmp;
-        fac[facnt++][1]=1;
-    }
+    if(tmp!=1){fac[facnt][0]=tmp;fac[facnt++][1]=1;}
     return facnt;
 }
