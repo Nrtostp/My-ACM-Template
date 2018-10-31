b83bbf1b39098dea8b7f35182f0f189ee819373b
qianyri <34699877+Nrtostp@users.noreply.github.com>
2018-10-31 20:59:17 +0800

    Update 13_Matrix_Tree.cpp

--- 01_Math/13_Matrix_Tree.cpp
+++ 01_Math/13_Matrix_Tree.cpp
@@ -5,24 +5,6 @@ void add(int x,int y)
     ++g[x][x];++g[y][y];
     --g[x][y];--g[y][x];
 }
-long long gauss()
-{
-    long long ans=1;
-    for(int i=1;i<n;++i)
-    {
-        for(int j=i+1;j<n;++j)
-        while(g[j][i])
-        {
-            long long t=g[i][i]/g[j][i];
-            for(int k=i;k<n;++k) g[i][k]=(g[i][k]-t*g[j][k]+MOD)%MOD;
-            swap(g[i],g[j]);
-            ans=-ans;
-        }
-        ans=(ans*g[i][i])%MOD;
-        if(!ans) return 0;
-    }
-    return (ans+MOD)%MOD;
-}
 /*minimum tree*/
 int n,m,p,g[MAX][MAX],vis[MAX],fa[MAX],ka[MAX];
 long long ans,mat[MAX][MAX];
