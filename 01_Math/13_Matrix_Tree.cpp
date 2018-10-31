db3137fdc33492b5c19e9b0281631c9eed019654
qianyri <34699877+Nrtostp@users.noreply.github.com>
2018-10-31 20:58:38 +0800

    Update 13_Matrix_Tree.cpp

--- 01_Math/13_Matrix_Tree.cpp
+++ 01_Math/13_Matrix_Tree.cpp
@@ -1,5 +1,4 @@
-const int MAX=1e2+5;
-const long long MOD=1e9+7;
+const int MAX=1e2+5;const long long MOD=1e9+7;
 long long g[MAX][MAX];
 void add(int x,int y)
 {
@@ -25,88 +24,76 @@ long long gauss()
     return (ans+MOD)%MOD;
 }
 /*minimum tree*/
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-const int N=101; 
-const int M=1001;
-ll n,m,p,ans;
-vector<int>gra[N];
-struct edge{int u,v,w;}e[M];
-int cmp(edge a,edge b){return a.w<b.w;}
-ll mat[N][N],g[N][N];
-ll fa[N],ka[N],vis[N];
-ll det(ll c[][N],ll n){
-    ll i,j,k,t,ret=1;
-    for(i=0;i<n;i++)
-    for(j=0;j<n;j++) c[i][j]%=p;
-    for(i=0; i<n; i++){
-        for(j=i+1; j<n; j++)
-            while(c[j][i]){
-                t=c[i][i]/c[j][i];
-                for(k=i; k<n; k++)
-                    c[i][k]=(c[i][k]-c[j][k]*t)%p;
-                swap(c[i],c[j]);
-                ret=-ret;
-            }
-        if(c[i][i]==0)
-            return 0L;
-        ret=ret*c[i][i]%p;
+int n,m,p,g[MAX][MAX],vis[MAX],fa[MAX],ka[MAX];
+long long ans,mat[MAX][MAX];
+vector<int>gra[MAX];
+struct P{int u,v,w;}e[MAX];
+bool cmp(P ta,P tb){return ta.w<tb.w;}
+long long gauss(int n)
+{
+    long long ans=1;
+    for(int i=0;i<n;++i)
+    {
+        for(int j=i+1;j<n;++j)
+        while(mat[j][i])
+        {
+            long long t=mat[i][i]/mat[j][i];
+            for(int k=i;k<n;++k) mat[i][k]=(mat[i][k]-t*mat[j][k]+p)%p;
+            swap(mat[i],mat[j]);
+            ans=-ans;
+        }
+        ans=(ans*mat[i][i])%p;
+        if(!ans) return 0;
     }
-    return (ret+p)%p;
+    return (ans+p)%p;
 }
-ll find(ll a,ll f[]){return f[a]==a?a:find(f[a],f);}
-void matrix_tree(){
-    for(int i=0;i<n;i++)if(vis[i]){
-        gra[find(i,ka)].push_back(i);
-        vis[i]=0;
-    }
-    for(int i=0;i<n;i++)
-    if(gra[i].size()>1){
-        memset(mat,0,sizeof mat);
+int find_(int x,int y[]) {return x==y[x]?x:find_(y[x],y);}
+void matrix_tree()
+{
+    for(int i=0;i<n;++i) if(vis[i]){gra[find_(i,ka)].push_back(i);vis[i]=0;}
+    for(int i=0;i<n;++i) if(gra[i].size()>1)
+    {
+        memset(mat,0,sizeof(mat));
         int len=gra[i].size();
-        for(int j=0;j<len;j++)
-        for(int k=j+1;k<len;k++){
+        for(int j=0;j<len;++j)
+        for(int k=j+1;k<len;++k)
+        {
             int u=gra[i][j],v=gra[i][k];
-            if(g[u][v]){
+            if(g[u][v])
+            {
                 mat[k][j]=(mat[j][k]-=g[u][v]);
                 mat[k][k]+=g[u][v];mat[j][j]+=g[u][v];
             }
         }
-        ans=ans*det(mat,gra[i].size()-1)%p;
-        for(int j=0;j<len;j++)fa[gra[i][j]]=i;//suo dian
+        ans=ans*gauss(gra[i].size()-1)%p;
+        for(int j=0;j<len;++j) fa[gra[i][j]]=i;
     }
-    for(int i=0;i<n;i++)
-    {
-        gra[i].clear();
-        ka[i]=fa[i]=find(i,fa);
-    }
+    for(int i=0;i<n;++i) {gra[i].clear();ka[i]=fa[i]=find_(i,fa);}
 }
-int main(){
-    while(scanf("%lld%lld%lld",&n,&m,&p),n){
-        for(int i=0;i<m;i++){
-            int u,v,w;
-            scanf("%d%d%d",&u,&v,&w);
-            u--;v--;
-            e[i]=(edge){u,v,w};
-        }
+int main()
+{
+    while(~scanf("%d%d%d",&n,&m,&p))
+    {
+        if(n==0&&m==0&&p==0) break;
+        memset(g,0,sizeof(g));ans=1;
+        for(int i=0;i<m;++i) {scanf("%d%d%d",&e[i].u,&e[i].v,&e[i].w);--e[i].u;--e[i].v;}
         sort(e,e+m,cmp);
-        memset(g,0,sizeof g);
-        ans=1;
-        for(ll i=0;i<n;i++)ka[i]=fa[i]=i;
-        for(ll i=0;i<=m;i++){
-            if(i&&e[i].w!=e[i-1].w||i==m)
-                matrix_tree();
-            ll u=find(e[i].u,fa),v=find(e[i].v,fa);
+        for(int i=0;i<n;++i) ka[i]=fa[i]=i;
+        for(int i=0;i<=m;++i)
+        {
+            if((i&&e[i].w!=e[i-1].w)||i==m) matrix_tree();
+            long long u=find_(e[i].u,fa),v=find_(e[i].v,fa);
             if(u!=v)
             {
                 vis[v]=vis[u]=1;
-                ka[find(u,ka)]=find(v,ka);
-                g[u][v]++,g[v][u]++;
+                ka[find_(u,ka)]=find_(v,ka);
+                ++g[u][v],++g[v][u];
             }
         }
         int flag=1;
-        for(int i=1;i<n;i++)if(fa[i]!=fa[i-1])flag=0;
+        for(int i=1;i<n;++i) if(fa[i]!=fa[i-1]) flag=0;
         printf("%lld\n",flag?ans%p:0);
     }
+    return 0;
 }
+
